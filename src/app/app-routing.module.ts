5a9f298e6d4e69e3b26bc3c7d3dc40814491404e
Mitchell Munson <MitchellSMunson@gmail.com>
2023-10-20 09:47:53 -0500

    Iniital commit - Will be updating more soon (such as .gitignore)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AdminComponent} from "./admin/admin.component";
+import {ProcessComponent} from "./process/process.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'admin', component: AdminComponent, children: [] },
+  { path: 'process', component: ProcessComponent, children: [] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
